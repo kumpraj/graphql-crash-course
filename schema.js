f51a4c7fff56b069356d5aba5d64d75fb56981ee
Prajwal Kumar <kumpraj@gmail.com>
2024-04-10 11:25:06 +0530

    added mutation for adding & deleting data(game)

--- schema.js
+++ schema.js
@@ -26,4 +26,12 @@ type Query {
    authors: [Author]
    author(id: ID!): Author
 }
+type Mutation {
+    addGame(game: AddGameInput!): Game
+    deleteGame(id: ID!): [Game]
+}
+input AddGameInput{
+    title: String!,
+    platform: [String!]!    
+}
 `
